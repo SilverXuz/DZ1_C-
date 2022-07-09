c68b330f3dcd51464bea62be1c2d6ebef0cea330
SilverXuz <silver-xuz@mail.ru>
2022-07-09 15:30:44 +0300

    Сравнение трех целых чисел

---
+++ Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите три целых числа. Нажмите Энтер после каждого из них");
+int a, b, c;
+a = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToInt32(Console.ReadLine());
+c = Convert.ToInt32(Console.ReadLine());
+if (a > b & a > c )
+{
+    Console.Write("Наибольшее число = ");
+    Console.WriteLine(a);
+}
+else
+{
+    if (b > c)
+    {
+        Console.Write("Наибольшее число = ");
+        Console.WriteLine(b);
+    }
+    else
+    {
+        Console.Write("Наибольшее число = ");
+        Console.WriteLine(c);
+    }
+}
