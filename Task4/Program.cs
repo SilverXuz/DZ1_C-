2a50d14f38839a96e1134d6bd5cf251d5aeb4e37
SilverXuz <silver-xuz@mail.ru>
2022-07-09 18:09:37 +0300

    Показывает четные числа от 1 до N

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,9 +1,9 @@
-﻿Console.Write("Введите целое число от 3 до 20: "); // Поставил лимит, чтобы исключить большие числа
+﻿Console.Write("Введите целое число от 1 до 20: "); // Поставил лимит, чтобы исключить большие числа
 int a = Convert.ToInt32(Console.ReadLine());
-while (a <= 2 || a > 20)
+while (a <= 0 || a > 20)
 {
     Console.WriteLine("Это число не подходит");
-        a = Convert.ToInt32(Console.ReadLine());
+    a = Convert.ToInt32(Console.ReadLine());
 }
 int i = 2;
 while (i <= a)
