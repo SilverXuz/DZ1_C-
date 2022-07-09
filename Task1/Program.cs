081dd3fc2f824f131375a8e042ebbab2e253dcf1
SilverXuz <silver-xuz@mail.ru>
2022-07-09 14:55:36 +0300

    Сравнение двух целых чисел

---
+++ Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите два целых числа. Нажмите Энтер после каждого из них");
+int a, b;
+a = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+    Console.Write("Наибольшее число = ");
+    Console.WriteLine(a);
+    Console.Write("Наименьшее число = ");
+    Console.WriteLine(b);
+}
+else
+{
+    Console.Write("Наибольшее число = ");
+    Console.WriteLine(b);
+    Console.Write("Наименьшее число = ");
+    Console.WriteLine(a);
+}
